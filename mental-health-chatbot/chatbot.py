39e82ba05b29e2392160f11fcc7e7843f84df8a3
김성현 <143778219+wis-hyun@users.noreply.github.com>
2024-06-19 17:54:50 +0900

    Add files via upload

---
+++ mental-health-chatbot/chatbot.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from streamlit_chat import message
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+
+@st.cache(allow_output_mutation=True)
+def cached_model():
+    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+    return model
+
+@st.cache(allow_output_mutation=True)
+def get_dataset():
+    df = pd.read_csv('/Users/sunghyunkim/Desktop/mental-health-chatbot-master/wellness_dataset.csv')
+    df['embedding'] = df['embedding'].apply(json.loads)
+    return df
+
+model = cached_model()
+df = get_dataset()
+
+st.header('눈송이 챗봇')
+
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+with st.form('form', clear_on_submit=True):
+    user_input = st.text_input('당신: ', '')
+    submitted = st.form_submit_button('전송')
+
+if submitted and user_input:
+    embedding = model.encode(user_input)
+
+    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+    answer = df.loc[df['distance'].idxmax()]
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(answer['챗봇'])
+
+for i in range(len(st.session_state['past'])):
+    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    if len(st.session_state['generated']) > i:
+        message(st.session_state['generated'][i], key=str(i) + '_bot')
+
+st.markdown(
+    f"""
+    <style>
+
+    [data-testid="stForm"] {{
+        background-color: lightblue; /* 파란색으로 변경 */
+        padding: 20px; 
+        border-radius: 10px; 
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
