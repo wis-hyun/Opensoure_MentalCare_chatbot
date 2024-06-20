c8a36b5abfb1084a7831aca505506bf678a6b503
ddasoll <ekthfmdk0212@sookmyung.ac.kr>
2024-06-21 02:51:41 +0900

    중복 폴더 제거, 소스 최신화

--- Opensoure_MentalCare_chatbot/mental_health_chatbot/chatbot_logic.py
+++
@@ -1,9 +0,0 @@
-def emergency_link():
-    answer = '#### 당신은 소중한 사람입니다. 포기하지마세요!\n'
-    answer += '🩵 [우울증과 우울감의 차이는?](https://kin.naver.com/open100/detail.nhn?d1id=7&dirId=70109&docId=1494755)\n'
-    answer += '🩵 [우울을 예방 및 극복하는 방법은?](https://kin.naver.com/open100/detail.nhn?d1id=7&dirId=70109&docId=1494757)\n'
-    answer += '🩵 [먼저 병원에 가야 하는 이유](https://kin.naver.com/open100/detail.nhn?d1id=7&dirId=70109&docId=1494766)\n'
-    answer += '🩵 [그래도, 삶은 희망이다](https://kin.naver.com/open100/detail.nhn?d1id=7&dirId=70109&docId=1494765)\n'
-
-    result = {'챗봇': answer}
-    return result
